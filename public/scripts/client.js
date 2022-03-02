68f80a9f336f63bb6c21a542bbb5ac71c0a889bf
Noah <lauffern@gmail.com>
2022-03-02 22:59:40 +0000

    feat: loading tweets after submitting via text area.

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,8 +1,8 @@
 /*
- * Client-side JS logic goes here
- * jQuery is already loaded
- * Reminder: Use (and do all your DOM work in) jQuery's document ready function
- */
+* Client-side JS logic goes here
+* jQuery is already loaded
+* Reminder: Use (and do all your DOM work in) jQuery's document ready function
+*/
 
 
 
@@ -11,57 +11,54 @@ const renderTweets = function(tweets) {
   // loops through tweets
   // calls createTweetElement for each tweet
   // takes return value and appends it to the tweets container
+  $('.dummy-tweets').empty();
+  
   for (let tweet of tweets){
     $('.dummy-tweets').prepend(createTweetElement(tweet));
   }
 }
 // creates tweets based on CSS styling 
 
-  const createTweetElement = function(tweet) {
-    let $tweet = $(` <article class="tweet">
-    <header>
-    <p><img class="bulb" src="${tweet.user.avatars}" alt="icon picture"> <span class="name">${tweet.user.name}<span></p>
-    <p class="username">${tweet.user.handle}</p>
-    </header>
-    <div>
-    <p><b>${tweet.content.text}</b></p>
-    </div>
+const createTweetElement = function(tweet) {
+  let $tweet = $(` <article class="tweet">
+  <header>
+  <p><img class="bulb" src="${tweet.user.avatars}" alt="icon picture"> <span class="name">${tweet.user.name}<span></p>
+  <p class="username">${tweet.user.handle}</p>
+  </header>
+  <div>
+  <p><b>${tweet.content.text}</b></p>
+  </div>
     <footer>
     <p>${timeago.format(tweet.created_at)}</p>
       <p class="icons"><i class="fa-solid fa-font-awesome"></i> <i class="fa-solid fa-retweet"></i> <i class="fa-solid fa-heart"></i></i></p>
-    </footer> `)
+      </footer> `)
     return $tweet;
   }
   
   
-  const formValidation = function () {
-
+  
+  const loadTweets = function() {
+  
+  
+    $.ajax({ 
+      method: "GET",
+      url: '/tweets',
+      dataType: 'JSON' })
+      .then(res => { 
+      renderTweets(res) 
+    })
+      .catch(err => {
+      console.log('err', err)
+    })
+  
   }
-  
-  
-  $(document).ready(function() {
-    
-    const loadTweets = function() {
-    
-    
-      $.ajax({ 
-        method: "GET",
-        url: '/tweets',
-        dataType: 'JSON' })
-        .then(res => { 
-        renderTweets(res) 
-      })
-        .catch(err => {
-        console.log('err', err)
-      })
-    
-    }
+
+  const postingTweets = function () {
 
     const $form =  $(".tweetSubmit")
-    $form.submit(function( event ) {
-  
+      $form.submit(function( event ) {
+    
       event.preventDefault();//stops post from happening 
-      let text = $( this ).serialize();
       const input = $("textarea").val();
       
       if (!input){
@@ -78,19 +75,30 @@ const renderTweets = function(tweets) {
         alert('invalid request: 140 character limit exceeded.')
         return;
       }
-     
+
+      let text = $( this ).serialize();
+  
       $.ajax({
         method: "POST",
         url: '/tweets',
         data: text
-      }).catch(err => {
+      }).then(data => {
+        loadTweets();
+      })
+        .catch(err => {
         console.log('err', err)
       })
-   
+      
+      
     });
 
+  }
+
+
+  $(document).ready(function() {
 
     loadTweets();
+    postingTweets();
 
 
 });
